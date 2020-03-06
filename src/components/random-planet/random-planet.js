b0556b3e5eb8cade2f7439da12bac6f8c340b5a0
Viktor Paliichuk <vpaliichuk@provectus.com>
2020-03-06 19:29:19 +0200

    created components with default content

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './random-planet.css';
+
+export default class RandomPlanet extends Component {
+
+    render() {
+
+
+        return (
+            <div className="random-planet jumbotron rounded">
+                <img className="planet-image"
+                     src={`https://starwars-visualguide.com/assets/img/planets/1.jpg`}/>
+                <div>
+                    <h4>wewewe we we</h4>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <span className="term">Population</span>
+                            <span>121</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Rotation Period</span>
+                            <span>4578</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Diameter</span>
+                            <span>5454789</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        );
+    }
+}
